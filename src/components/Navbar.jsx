1715bba775142e9ed652119397a432e0f3e4ae82
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-07-12 20:01:02 +0530

    fix: fixed bug in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { FiUserPlus } from 'react-icons/fi'
+import { AiOutlineHeart, AiOutlineShoppingCart } from 'react-icons/ai'
+import { FiUserMinus, FiUserPlus } from 'react-icons/fi'
 import { BsList } from 'react-icons/bs'
 import { SideNavBar } from './SideNavBar'
 
 export const Navbar = () => {
 
   const [showSideBar,setShowSideBar] = useState(false)
+  const user = false
 
   function handleSideBar(){
     setShowSideBar(prevState => !prevState)
@@ -41,32 +42,54 @@ export const Navbar = () => {
               Products
             </li>
           </NavLink>
-        </ul>
-        <ul 
-          className='hidden lg:flex items-center font-poppins tracking-wider text-[1.5rem]'>
-          <NavLink to='/cart'
-            className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
+          {user && <NavLink 
+            to='/profile' 
+            className={(navData) => navData.isActive ? 'underline font-semibold text-primary' : ''}>
             <li 
-              className='mr-4 hover:text-primaryHover flex items-center'>
-              <p className='mr-2'>Cart</p>
-              <AiOutlineShoppingCart/>
+              className='mr-8 hover:text-primaryHover'>
+              Profile
             </li>
-          </NavLink>
-          <NavLink to='/login'
-            className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
-            <li 
-              className='mr-4 hover:text-primaryHover flex items-center'>
-              <p className='mr-2'>Login</p>
-              <FiUserPlus/>
-            </li>
-          </NavLink>
+          </NavLink>}
         </ul>
+        {user 
+          ? <ul 
+              className='hidden lg:flex items-center font-poppins tracking-wider text-[1.3rem]'>
+              <NavLink to='/cart'
+                className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
+                <li 
+                  className='mr-4 hover:text-primaryHover'>
+                  <AiOutlineShoppingCart/>
+                </li>
+              </NavLink>
+              <NavLink to='/cart'
+                className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
+                <li 
+                  className='mr-4 hover:text-primaryHover'>
+                  <AiOutlineHeart/>
+                </li>
+              </NavLink>
+              <li className='mr-4 flex items-center text-[1rem] cursor-pointer '>
+                <button className='flex items-center border-2 border-primary px-4 py-2 rounded-md hover:bg-primary hover:text-white transition-all'>
+                  <span>Logout</span>
+                  <FiUserMinus className='ml-2'/>
+                </button>                  
+              </li>
+            </ul>
+            : <NavLink to='/login'
+                className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover hidden lg:block' : 'hidden lg:block'}>
+                <buton 
+                  className='text-xl flex items-center border-2 border-primary px-4 py-2 rounded-md hover:bg-primary hover:text-white transition-all'>
+                  <p className='mr-2'>Login</p>
+                  <FiUserPlus/>
+                </buton>
+              </NavLink>
+            }
         <BsList 
           size='40px' 
           color='#7E74F8' 
           className='block lg:hidden cursor-pointer'
           onClick={handleSideBar}
-          />
+        />
       </div>
       {showSideBar && <SideNavBar handleSideBar={handleSideBar}/>}
     </>
--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { FiUserPlus } from 'react-icons/fi'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { AiOutlineHeart, AiOutlineShoppingCart } from 'react-icons/ai'
 import { ImCross } from 'react-icons/im'
 
 
 export const SideNavBar = (props) => {
+
+    const user = false;
+
   return (
     <div className='w-full h-full fixed top-4 left-0 bg-[#F0F0F0] z-50'>
         <div className='flex justify-between px-10'>
@@ -43,32 +46,63 @@ export const SideNavBar = (props) => {
                         Products
                     </li>
                 </NavLink>
+                {user && <NavLink 
+                to='/profile' 
+                className={(navData) => navData.isActive ? 'font-semibold text-secondary' : ''}>
+                    <li 
+                        className='mt-4 hover:p-4 hover:text-primary hover:bg-ternary transition-all'
+                        onClick={props.handleSideBar}
+                    >
+                        Profile
+                    </li>
+                </NavLink>}
+                {user && <li 
+                    className='mt-4 hover:p-4 hover:text-primary hover:bg-ternary transition-all'
+                    onClick={props.handleSideBar}
+                >
+                    Logout
+                </li>}
             </ul>
         </div>
         <div className='w-full mx-auto mt-8'>
-            <ul 
-                className='flex justify-center items-center font-poppins tracking-wider text-[1.5rem]'>
-                <NavLink to='/cart'
-                    className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
-                    <li 
-                        className='mr-4 hover:text-primaryHover flex items-center'
-                        onClick={props.handleSideBar}
-                    >
-                            <p className='mr-2'>Cart</p>
-                        <AiOutlineShoppingCart/>
-                    </li>
-                </NavLink>
-                <NavLink to='/login'
-                    className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
-                    <li 
-                        className='mr-4 hover:text-primaryHover flex items-center'
-                        onClick={props.handleSideBar}    
-                    >
-                        <p className='mr-2'>Login</p>
-                        <FiUserPlus/>
-                    </li>
-                </NavLink>
-            </ul>
+            {user 
+                ? <ul 
+                    className='flex justify-center items-center font-poppins tracking-wider text-[1.5rem]'>
+                    <NavLink to='/cart'
+                        className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
+                        <li 
+                            className='mr-4 hover:text-primaryHover flex items-center'
+                            onClick={props.handleSideBar}
+                        >
+                                <p className='mr-2'>Cart</p>
+                            <AiOutlineShoppingCart/>
+                        </li>
+                    </NavLink>
+                    <NavLink to='/wishlist'
+                        className={(navData) => navData.isActive ? 'underline font-semibold text-primaryHover' : ''}>
+                        <li 
+                            className='mr-4 hover:text-primaryHover flex items-center'
+                            onClick={props.handleSideBar}    
+                        >
+                            <p className='mr-2'>WishList</p>
+                            <AiOutlineHeart/>
+                        </li>
+                    </NavLink>
+                </ul>
+                : <div className='flex items-center justify-center'>
+                    <NavLink to='/login'
+                        className={(navData) => navData.isActive ? 'font-semibold text-primaryHover' : ''}>
+                        <button 
+                            className='mr-4 flex items-center tracking-wider text-[1.5rem] border-primary border-2 px-4 py-2 rounded-md hover:bg-primary hover:text-white transition-all'
+                            onClick={props.handleSideBar}    
+                        >
+                            <p className='mr-2'>Login</p>
+                            <FiUserPlus/>
+                        </button>
+                    </NavLink>
+                </div>
+               
+            }
         </div>
     </div>
   )
